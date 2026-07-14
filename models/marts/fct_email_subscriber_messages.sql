03c146bae15b2925676316ecd359221772d85699
Tim Finkel <tim.finkel@gmail.com>
2026-07-14 16:18:15 -0400

    Clarify subscriber message aggregate names

--- models/marts/fct_email_subscriber_messages.sql
+++ models/marts/fct_email_subscriber_messages.sql
@@ -3,11 +3,11 @@ select
     as email_subscriber_message_id,
     email_id,
     subscriber_id,
-    min(if(event_type = "sent", event_ts, null)) as sent_ts,
-    min(if(event_type = "delivered", event_ts, null)) as delivered_ts,
+    min(if(event_type = "sent", event_ts, null)) as min_sent_ts,
+    min(if(event_type = "delivered", event_ts, null)) as min_delivered_ts,
     min(if(event_type = "opened", event_ts, null)) as min_opened_ts,
     min(if(event_type = "clicked", event_ts, null)) as min_clicked_ts,
-    countif(event_type = "opened") as open_event_count,
-    countif(event_type = "clicked") as click_event_count
+    countif(event_type = "opened") as n_events_opened,
+    countif(event_type = "clicked") as n_events_clicked
 from {{ ref("fct_email_events") }}
 group by email_id, subscriber_id
